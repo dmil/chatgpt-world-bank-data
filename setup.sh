bf62cd23035b00204e75f975330af3628c85acbb
Dhrumil Mehta <dhrumil.mehta@gmail.com>
2023-11-13 17:19:26 -0500

    update setup.sh

---
+++ setup.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo apt-get install -y r-base
+sudo apt-get install -y libharfbuzz-dev libfribidi-dev libfreetype6-dev libpng-dev libtiff5-dev libjpeg-dev
+sudo Rscript setup.R
+pip install -r requirements.txt
